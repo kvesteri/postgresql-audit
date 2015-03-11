ea2e3e38cbd7bfeed1122af05a6a0464bb2d6e64
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2015-03-11 12:47:24 +0200

    Remove HSTORE extension requirement and comments

--- postgresql_audit/schema.sql
+++ postgresql_audit/schema.sql
@@ -1,19 +1,3 @@
--- An audit history is important on most tables. Provide an audit trigger that logs to
--- a dedicated audit table for the major relations.
---
--- This file should be generic and not depend on application roles or structures,
--- as it's being listed here:
---
---    https://wiki.postgresql.org/wiki/Audit_trigger_91plus
---
--- This trigger was originally based on
---   http://wiki.postgresql.org/wiki/Audit_trigger
--- but has been completely rewritten.
---
--- Should really be converted into a relocatable EXTENSION, with control and upgrade files.
-
-CREATE EXTENSION IF NOT EXISTS hstore;
-
 CREATE SCHEMA audit;
 REVOKE ALL ON SCHEMA audit FROM public;
 
