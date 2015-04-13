55bb10224c9edf47c23b1d8bcdf49b23d0b719e9
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2015-04-13 14:01:26 +0300

    Add tests for jsonb_merge

--- tests/test_json_functions.py
+++ tests/test_json_functions.py
@@ -1,7 +1,7 @@
 import pytest
 import sqlalchemy as sa
 
-from postgresql_audit import jsonb_change_key_name
+from postgresql_audit import jsonb_change_key_name, jsonb_merge
 
 
 @pytest.mark.usefixtures('activity_cls', 'table_creator')
@@ -32,7 +32,7 @@ class TestJSONBChangeKeyName(object):
     def test_raw_sql(self, session, data, old_key, new_key, expected):
         result = session.execute(
             '''SELECT jsonb_change_key_name(
-                '{data}',
+                '{data}'::jsonb,
                 '{old_key}',
                 '{new_key}'
             )'''.format(
@@ -76,5 +76,72 @@ class TestJSONBChangeKeyName(object):
     ):
         result = session.execute(
             sa.select([jsonb_change_key_name(data, old_key, new_key)])
+        ).scalar()
+        assert result == expected
+
+
+@pytest.mark.usefixtures('activity_cls', 'table_creator')
+class TestJSONBMerge(object):
+    @pytest.mark.parametrize(
+        ('data', 'merge_data', 'expected'),
+        (
+            (
+                '{"key1": 4, "key2": 3}',
+                '{"key1": 5, "key3": 5}',
+                {"key1": 5, "key2": 3, "key3": 5}
+            ),
+            (
+                '{}',
+                '{"key1": 5, "key3": 5}',
+                {"key1": 5, "key3": 5}
+            ),
+            (
+                '{"key1": 4, "key2": 3}',
+                '{}',
+                {"key1": 4, "key2": 3}
+            ),
+        )
+    )
+    def test_raw_sql(self, session, data, merge_data, expected):
+        result = session.execute(
+            '''SELECT jsonb_merge(
+                '{data}'::jsonb,
+                '{merge_data}'::jsonb
+            )'''.format(
+                data=data,
+                merge_data=merge_data
+            )
+        ).scalar()
+        assert result == expected
+
+    @pytest.mark.parametrize(
+        ('data', 'merge_data', 'expected'),
+        (
+            (
+                '{"key1": 4, "key2": 3}',
+                '{"key1": 5, "key3": 5}',
+                {"key1": 5, "key2": 3, "key3": 5}
+            ),
+            (
+                '{}',
+                '{"key1": 5, "key3": 5}',
+                {"key1": 5, "key3": 5}
+            ),
+            (
+                '{"key1": 4, "key2": 3}',
+                '{}',
+                {"key1": 4, "key2": 3}
+            ),
+        )
+    )
+    def test_sqlalchemy_function_expr(
+        self,
+        session,
+        data,
+        merge_data,
+        expected
+    ):
+        result = session.execute(
+            sa.select([jsonb_merge(data, merge_data)])
         ).scalar()
         assert result == expected
